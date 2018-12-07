740ef588062b6a8555b741c0cb95233b2fb129de
Rafael Luque <rafael.luque@osoco.es>
2018-12-07 08:56:48 +0000

    Start simple HTTP server based on Python

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ cargo build --release --target wasm32-unknown-unknown
 sudo chown -R $(id -u):$(id -g) target html
 cp target/wasm32-unknown-unknown/release/osoco_xmas2018.wasm html/gate_app.wasm
 cp js/howler.js html
+cd html
+python -m SimpleHTTPServer &
