a62f14611bf976383136678b80c893fbba9c7617
Rafael Luque <rafael.luque@osoco.es>
2019-01-04 09:57:11 +0000

    Change html directory to docs as it is required by GitHub Pages mechanism

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@ PATH=$PATH:$HOME/.cargo/bin
 RUST_BACKTRACE=1
 rustup target add wasm32-unknown-unknown
 cargo build --release --target wasm32-unknown-unknown
-sudo chown -R $(id -u):$(id -g) target html
-cp target/wasm32-unknown-unknown/release/osoco_xmas2018.wasm html/gate_app.wasm
-cp js/*.js html
-cd html
+cp -R html/* docs
+sudo chown -R $(id -u):$(id -g) target docs
+cp target/wasm32-unknown-unknown/release/osoco_xmas2018.wasm docs/gate_app.wasm
+cp js/*.js docs
+sudo rm -rf html
+cd docs
 python -m SimpleHTTPServer
